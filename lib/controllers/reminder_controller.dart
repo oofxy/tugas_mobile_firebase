ffbf222fc8cd884a661848fee5d8faaa43c69c9f
Zaki Firasi <zaki.firasi@gmail.com>
2025-01-11 14:40:41 +0700

    Feat : [ZF] set reminder functionality and push notification

---
+++ lib/controllers/reminder_controller.dart
@@ -0,0 +1,23 @@
+import 'notification_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+
+class ReminderController extends GetxController {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final NotificationController _notifController = Get.find();
+
+  Future<void> addReminder(DateTime dateTime, String description) async {
+    try {
+      await _firestore.collection('reminders').add({
+        "timestamp": Timestamp.fromDate(dateTime),
+        "description": description,
+        "userId": _notifController.token,
+        "isSent": false
+      });
+      Get.snackbar("Success", "Reminder added succesfully");
+    } catch (e) {
+      Get.snackbar("Error", "Failed to add reminder: $e");
+    }
+  }
+}
---
+++ lib/pages/reminder_form.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import '../controllers/reminder_controller.dart';
+
+class ReminderForm extends StatelessWidget {
+  final ReminderController _controller = Get.find();
+  final TextEditingController _descriptionController = TextEditingController();
+  final Rxn<DateTime> _selectedDateTime = Rxn<DateTime>();
+
+  void _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100)
+    );
+    if (pickedDate != null) {
+      final TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+      );
+      if (pickedTime != null) {
+        _selectedDateTime.value = DateTime(
+            pickedDate.year,
+            pickedDate.month,
+            pickedDate.day,
+            pickedTime.hour,
+            pickedTime.minute
+        );
+      }
+    }
+  }
+
+  void _submitForm() {
+    if (_selectedDateTime.value != null && _descriptionController.text.isNotEmpty) {
+      _controller.addReminder(_selectedDateTime.value!, _descriptionController.text);
+      _descriptionController.clear();
+      _selectedDateTime.value = null;
+    } else {
+      Get.snackbar("Error", "Please fill out all fields");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Reminder")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: "Description"),
+            ),
+            SizedBox(height: 20),
+            Obx(() => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _selectedDateTime.value != null
+                      ? DateFormat.yMd().add_jm().format(_selectedDateTime.value!)
+                      : "No date selected",
+                ),
+                TextButton(
+                  onPressed: () => _selectDateTime(context),
+                  child: Text("Select Date & Time"),
+                ),
+              ],
+            )),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: Text("Add Reminder"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
