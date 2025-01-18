43d213a1194dd2ef80c2465a55c8afe2da8c86b7
Zaki Firasi <giggs.samestagiga+github@gmail.com>
2025-01-18 20:42:14 +0700

    Feat:[ZF] setup used colors and fonts

---
+++ lib/style/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const primary = Color(0x0D0183B7);
+  static const primaryHighlighted = Color(0x260183B7);
+  static const onPrimary = Color(0x800183B7);
+
+  // Secondary
+  static const secondary = Color(0x0D777777);
+
+  // Tertiary
+  static const tertiary = Color(0xFF0183B7);
+  static const onTertiary = Color(0xFFFFFFFF);
+
+  // Warning
+  static const warning = Color(0xFFFF0000);
+  static const warningText = Color(0x99FF0000);
+
+  // Text
+  static const textPrimary = Color(0xFF000000);
+  static const textSecondary = Color(0x80000000);
+
+  // Border
+  static const borderPrimary = Color(0x0D0183B7);
+  static const borderPrimaryHighlighted1 = Color(0x260183B7);
+  static const borderPrimaryHighlighted2 = Color(0x660183B7);
+  static const borderSecondary = Color(0x0D0183B7);
+  static const borderTertiary = Color(0x1AFFFFFF);
+}
